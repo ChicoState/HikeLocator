245294d2928f795ee064146a79ee18484214703d
Emily Mckillican <emilymckillican@wlan-t-105-94-dhcp.CSUChico.EDU>
2019-05-02 16:32:04 -0700

    post flutter clean

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,14 +3,32 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-#import <path_provider/PathProviderPlugin.h>
-#import <sqflite/SqflitePlugin.h>
+#import <cloud_firestore/CloudFirestorePlugin.h>
+#import <firebase_auth/FirebaseAuthPlugin.h>
+#import <firebase_core/FirebaseCorePlugin.h>
+#import <firebase_database/FirebaseDatabasePlugin.h>
+#import <flutter_share_me/FlutterShareMePlugin.h>
+#import <fluttertoast/FluttertoastPlugin.h>
+#import <geolocator/GeolocatorPlugin.h>
+#import <google_api_availability/GoogleApiAvailabilityPlugin.h>
+#import <google_maps_flutter/GoogleMapsPlugin.h>
+#import <permission_handler/PermissionHandlerPlugin.h>
+#import <url_launcher/UrlLauncherPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
-  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [FLTCloudFirestorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTCloudFirestorePlugin"]];
+  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FLTFirebaseDatabasePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseDatabasePlugin"]];
+  [FlutterShareMePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterShareMePlugin"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [GeolocatorPlugin registerWithRegistrar:[registry registrarForPlugin:@"GeolocatorPlugin"]];
+  [GoogleApiAvailabilityPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleApiAvailabilityPlugin"]];
+  [FLTGoogleMapsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleMapsPlugin"]];
+  [PermissionHandlerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionHandlerPlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
 }
 
 @end
